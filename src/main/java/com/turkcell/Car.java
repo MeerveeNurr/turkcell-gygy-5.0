c9af8c8b91234b6cbebfecc9e9901344ece01700
MeerveeNurr <mervenurrkk@gmail.com>
2026-04-09 00:33:23 +0300

    oop giris

--- src/main/java/com/turkcell/Car.java
+++ src/main/java/com/turkcell/Car.java
@@ -4,13 +4,33 @@ public class Car
 {
     //GERÇEK HAYATTAKİ HER ŞEYİ DEĞİL,
     //PROGRAMDA İHTİYACIMIZ OLAN ŞEYLERİ MODELLEMEK İSTİYORUZ.
+    public int year;
     public String model;
     public String brand;
-    public int year;
-    // Public->her noktadan erişilebilir alan.
+    private double price;
+
+     //Erişim beirteci->o alana kimlerin erişebileceğini belirleyen sistem.
+    // Public->her yerden erişilebilir alan.
+    //private->sadece tanımlandığı sınıfın içinden erişilebilir alan.
+    //protected->aynı paketteki sınıflardan erişilebilir.Sadece tanımlanan sınıf ve o  sınıftan(türetilen) sınıflardan erişilebilir.
 
     //ENCAPSULATION(KAPSÜLLEME)
     //Read-only write-only
+
+    // price değerini private yaptık, get ve set ile erişim sağlandı.ama doğrudan değil.
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price){
+        //classın kendisi ->price
+        if(price<0){
+            System.out.println("Fiyat sıfırdan küçük olamaz.")
+            return;
+        }
+        this.price=price;
+    }
+
+
     private Double pricePerDay;
 // get set metodunu hocanın videoyla kıyasl
     public String getModel() {
--- src/main/java/com/turkcell/OOP.java
+++ src/main/java/com/turkcell/OOP.java
@@ -2,12 +2,23 @@ package com.turkcell;
 
 public class OOP {
  public static void main(String[] args) {
-    Car car1 = new Car(); // new Car();
+    Car car1 = new Car(); // new Car(); new -> yeni bir instance oluşturma keyword'ü, Car() ise Car sınıfının constructor'ı. Constructor, bir sınıftan nesne oluşturmak için kullanılan özel bir metottur. Car sınıfında constructor tanımlanmazsa, Java varsayılan bir constructor sağlar.
+
+    car1.year = 2020; 
+    car1.model = "X5"; 
+    car1.brand = "BMW"; 
+    //car1.price = 500.000; // set işlemi değer atama işlemidir. car1 nesnesinin price özelliğine 500.000 değerini atadık.
+
+    car1.setPrice(500.000); //set 
+
+    System.out.println(car1.getPrice()); // get işlemi değer okuma işlemidir. car1 nesnesinin price özelliğinin değerini ekrana yazdırır.
+   
+
     //yeni bir araba nesnesi örneği(instance) üret
     car1.setBrand("BMV");
     car1.setModel("X5");
     car1.setYear(2020);
-    car1.setPricePerDay(-500.0);
+    car1.setPricePerDay(-500.0); 
 
     System.out.println(car1.getBrand());// get işlemi değer
     System.out.println(car1.getPricePerDay()); //private...

cd9796d58385513a52b03134e985f7827225b6d7
MeerveeNurr <mervenurrkk@gmail.com>
2026-04-10 16:40:13 +0300

    düzenleme

--- bankingapplication/src/main/java/com/turkcell/bankinterface.java
+++
@@ -1,9 +0,0 @@
-package com.turkcell;
-public interface BankInterface {
-    public void paraYatir(int miktar);
-    public void paraCek(int miktar);
-    public void bakiyeOgren();
-    public void kartIade();
-    public boolean sifreKontrol(String sifre);
-
-}
